496a4b8bf2f524c0a7a171b8e46084c1d1bd61b7
David Kahara <kaharadave@gmail.com>
2019-02-12 09:07:26 +0300

    feat(registration-redux): Implement API call and redux configuration
    -Write tests for functionality
    -Add redux/thunk
    -Add validation
    
    [Delivers #162948801]

--- src/store/modules/signup/types.js
+++ src/store/modules/signup/types.js
@@ -1,4 +1,4 @@
 /* eslint-disable import/prefer-default-export */
-export const SIGNUP_REQUEST = 'SIGNUP_REQUEST';
-export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
-export const SIGNUP_FAILURE = 'SIGNUP_FAILURE';
+export const SIGNUP_REQUEST = "SIGNUP_REQUEST";
+export const SIGNUP_SUCCESS = "SIGNUP_SUCCESS";
+export const SIGNUP_FAILURE = "SIGNUP_FAILURE";
