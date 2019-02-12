f91e42a3c21512f0646da297fc4ceebefaf49a8a
kwame asiago <kwameasiago@gmail.com>
2019-02-12 19:30:48 +0300

    feat(Profile) Create a profile page - Add a page for profile - Add nvaigation bar component [Delivers #162948805]

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Navbar from '../components/Navbar';
+
+
+class Profile extends Component {
+    render() {
+        return (
+            <Navbar />
+        );
+    };
+}
+
+export default Profile;
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from '../pages/Home';
 import Signup from '../pages/Signup';
+import Profile from '../pages/Profile';
+import Login from '../components/Login';
 
 const Routes = () => {
   return (
@@ -9,6 +11,8 @@ const Routes = () => {
       <Switch>
         <Route exact path='/' component={Home} />
         <Route signup path='/signup' component={Signup} />
+        <Route path='/login' component={ Login } />
+        <Route path='/profile' component={ Profile }></Route>
       </Switch>
     </BrowserRouter>
   );
