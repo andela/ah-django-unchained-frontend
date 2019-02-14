4632e4e0fc0784a18a645ee24cec97daba0f3d53
KMaina <maina.ken0@gmail.com>
2019-02-14 14:08:31 +0300

    feat(login): fix linting errors
    [Delivers #162948803]

--- src/components/Button/button.test.jsx
+++ src/components/Button/button.test.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import { shallow } from "enzyme";
-import Button from "./";
+import React from 'react';
+import { shallow } from 'enzyme';
+import Button from './';
 
-describe("Button", () => {
-  it("should be defined", () => {
+describe('Button', () => {
+  it('should be defined', () => {
     expect(Button).toBeDefined();
   });
-  it("should render correctly", () => {
+  it('should render correctly', () => {
     const tree = shallow(
       <Button
-        type="submit"
-        className="col-sm-6 btn btn-success form-control"
-        text="login"
+        type='submit'
+        className='col-sm-6 btn btn-success form-control'
+        text='login'
       />
     );
     expect(tree).toMatchSnapshot();
--- src/components/InputBox/inputbox.test.jsx
+++ src/components/InputBox/inputbox.test.jsx
@@ -1,19 +1,19 @@
-import React from "react";
-import { shallow } from "enzyme";
-import InputBox from "./";
+import React from 'react';
+import { shallow } from 'enzyme';
+import InputBox from './';
 
-describe("InputBox", () => {
-  it("should be defined", () => {
+describe('InputBox', () => {
+  it('should be defined', () => {
     expect(InputBox).toBeDefined();
   });
-  it("should render correctly", () => {
+  it('should render correctly', () => {
     const tree = shallow(
-        <InputBox
-        type="password"
-        name="password"
-        id="password"
-        placeholder="password"
-        className="col-sm-6"
+      <InputBox
+        type='password'
+        name='password'
+        id='password'
+        placeholder='password'
+        className='col-sm-6'
       />
     );
     expect(tree).toMatchSnapshot();
