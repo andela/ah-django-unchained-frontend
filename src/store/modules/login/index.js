bc31caeade7be94f457488cc036307d388ea3511
KMaina <maina.ken0@gmail.com>
2019-02-15 10:43:44 +0300

    feat(login): add reducers for login
    - Create a component for login
    - Create reducer for login
    - Consume the login endpoint
    
    [Delivers #162948803]

---
+++ src/store/modules/login/index.js
@@ -0,0 +1,47 @@
+import { LOGIN_ERROR, LOGIN_SUCCESS } from './types';
+import axios from 'axios';
+
+const initialState = {};
+
+export const logginSuccessAction = response => ({
+  type: LOGIN_SUCCESS,
+  response
+});
+
+export const logginErrorAction = response => ({
+  type: LOGIN_ERROR,
+  response
+});
+
+export const loginUser = userData => dispatch => {
+  axios
+    .post('http://127.0.0.1:8000/api/users/login/', {
+      user: userData
+    })
+    .then(res => {
+      console.log(res);
+      dispatch(logginSuccessAction(res));
+    })
+    .catch(errors => {
+      console.log(errors);
+      dispatch(logginErrorAction(errors));
+    });
+};
+
+export const loginReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOGIN_SUCCESS:
+      localStorage.setItem('token', action.payload.token);
+      return {
+        response: action.payload
+      };
+    case LOGIN_ERROR:
+      return {
+        errors: action.payload
+      };
+    default:
+      return state;
+  }
+};
+
+export default loginReducer;
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux';
 import { signUpReducer } from './modules/signup/index';
+import {loginReducer} from './modules/login/index';
 
 const rootReducer = combineReducers({ 
-    signUpReducer,
+    signUpReducer, loginReducer
 });
 
 export default rootReducer;
