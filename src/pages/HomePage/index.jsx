1b8e26a5580ac11034aabd8383e9e6ae19710529
Georgeygigz <georgeymutti@gmail.com>
2019-02-22 11:49:18 +0300

    feat(Get all articles):Get all articles
    - Get all articles
    - Write test for getting all articles
    
    [Starts #162948806]

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getallArticles } from '../../store/modules/getArticles/index';
+import Loader from '../../components/Loader/index';
 import ArticlePanel from '../../components/ArticlePanel/index';
 
-const Home = () => {
+export class HomePage extends Component {
+  componentDidMount() {
+    const { getallArticles } = this.props;
+    getallArticles();
+  }
+
+  render() {
+    const { message } = this.props;
+    const articles = this.props;
     return (
       <div className="container">
-        <ArticlePanel />
-        <ArticlePanel />
-        <ArticlePanel />
+        {message === 'fetching' ? (
+          <Loader />
+        ) : articles.articles.length ? (
+          articles.articles.map(article => {
+            return <ArticlePanel article={article} />;
+          })
+        ) : (
+          <div className="container">
+            <h1>No articles</h1>
+          </div>
+        )}
       </div>
     );
-  };
+  }
+}
 
-export default Home;
+export const mapStateToProps = state => ({
+  articles: state.articleReducer.articles,
+  error: state.articleReducer.error,
+  message: state.articleReducer.message
+});
+
+export const mapDispatchToProps = dispatch => ({
+  getallArticles: () => dispatch(getallArticles())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(HomePage);
---
+++ src/store/modules/getArticles/index.js
@@ -0,0 +1,67 @@
+import * as types from './types';
+import http from '../../../utils/helpers/http';
+
+const initialState = {
+  articles: [],
+  message: ''
+};
+
+export const getAllArticlesRequest = () => ({
+  type: types.GET_ALL_ARTICLES_REQUEST,
+  payload: {
+    fetching: true,
+    message: 'fetching'
+  }
+});
+
+export const getAllArticlesSuccess = article => ({
+  type: types.GET_ALL_ARTICLES_SUCCESS,
+  payload: {
+    article,
+    message: 'success'
+  }
+});
+
+export const getAllArticlesFailure = error => ({
+  type: types.GET_ALL_ARTICLES_FAILURE,
+  payload: {
+    error,
+    fetching: false,
+    message: 'failure'
+  }
+});
+
+export const getallArticles = () => dispatch => {
+  dispatch(getAllArticlesRequest());
+  return http
+    .get('/api/articles/')
+    .then(res => {
+      dispatch(getAllArticlesSuccess(res.data.results));
+    })
+    .catch(error => {
+      dispatch(getAllArticlesFailure(error));
+    });
+};
+
+export const getAllArticleReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case types.GET_ALL_ARTICLES_SUCCESS:
+      return {
+        ...state,
+        articles: action.payload.article,
+        message: action.payload.message
+      };
+    case types.GET_ALL_ARTICLES_REQUEST:
+      return { ...state, ...action.payload };
+
+    case types.GET_ALL_ARTICLES_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        message: action.payload.message
+      };
+    default:
+      return state;
+  }
+};
+export default getAllArticleReducer;
