396da43b2386da8bc2beea81635975c223fbe03a
Margaret Chege <ciku@MacBook-Pro-4.local>
2019-02-26 14:12:53 +0300

    bg(fix article panel): Add props(isPublished) Can be used for view draft articles and published articles. Include image in component [Delivers #164241109]

--- src/components/ArticlePanel/index.jsx
+++ src/components/ArticlePanel/index.jsx
@@ -1,50 +1,55 @@
 import React from 'react';
+import TruncateMarkup from 'react-truncate-markup';
 import { FaStar, FaThumbsUp, FaThumbsDown } from 'react-icons/fa';
+import './ArtclePanel.scss';
+import ReactHtmlParser from 'react-html-parser';
 
-const ArticlePanel = ({ article }) => {
+const ArticlePanel = ({ article, isPublished }) => {
   return (
-    <div className="container" key={article.id}>
-      <div className="card bg-secondary text-white">
+    <div className="container">
+    <div className="col-sm-8" key={article.id}>
+      <div className="card">
         <div className="card-body">
-          <h4 className="card-title">{article.title}</h4>
-          <a href="/" className="card-link text-warning">
-            <FaStar />
-          </a>
-          <a href="/" className="card-link text-warning">
-            <FaStar />
-          </a>
-          <a href="/" className="card-link text-warning">
-            <FaStar />
-          </a>
-          <p className="card-text">{article.body}</p>
-          <a href="/" className="card-link btn btn-info">
-            <FaThumbsUp />
-          </a>
-          <a href="/" className="card-link btn btn-info">
-            <FaThumbsDown />
-          </a>
-          <a href="/" className="card-link btn btn-info">
-            Read
-          </a>
-          <a href="/" className="card-link btn btn-info">
-            Bookmark
-          </a>
-          <span className="card-link"> Rating </span>
-          <a href="/" className="card-link text-warning">
-            <FaStar />
-          </a>
-          <a href="/" className="card-link text-warning">
-            <FaStar />
-          </a>
-          <a href="/" className="card-link text-warning">
-            <FaStar />
-          </a>
-          <a href="/" className="card-link text-warning">
-            <FaStar />
-          </a>
+          
+            <img  src={article.images || 'https://cdn.shopify.com/s/assets/no-image-2048-5e88c1b20e087fb7bbe9a3771824e743c244f437e4f8ba93bbf7b11b53f7824c_1024x1024.gif'} alt='' />
+              <h4 className="card-title">{article.title}</h4>
+              <TruncateMarkup lines={2}>
+                <p className="card-text">{ReactHtmlParser(article.body)}</p>
+              </TruncateMarkup>
+              {isPublished && (
+                <React.Fragment>
+                  <a href="/" className="card-link btn btn-info">
+                    <FaThumbsUp />
+                  </a>
+                  <a href="/" className="card-link btn btn-info">
+                    <FaThumbsDown />
+                  </a>
+                  <a href="/" className="card-link btn btn-info">
+                  Read
+                  </a>
+                  <a href="/" className="card-link btn btn-info">
+                  Bookmark
+                  </a>
+                  <span className="card-link"> Rating </span>
+                  <a href="/" className="card-link text-warning">
+                    <FaStar />
+                  </a>
+                  <a href="/" className="card-link text-warning">
+                    <FaStar />
+                  </a>  
+                  <a href="/" className="card-link text-warning">
+                    <FaStar />
+                  </a>
+                  <a href="/" className="card-link text-warning">
+                    <FaStar />
+                  </a>
+                </React.Fragment>
+            )}
+          
         </div>
       </div>
     </div>
+    </div>  
   );
 };
 
