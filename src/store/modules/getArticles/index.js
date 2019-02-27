8314a66cce34f693c1ee8d42e847d4a5282639d6
David Kahara <kaharadave@gmail.com>
2019-02-27 10:48:59 +0300

    chore(http): Create reusable axios instances
    -Create an axios instance with token
    
    [Finishes #164267095]

--- src/store/modules/getArticles/index.js
+++ src/store/modules/getArticles/index.js
@@ -1,5 +1,5 @@
 import * as types from './types';
-import http from '../../../utils/helpers/http';
+import {http} from '../../../utils/helpers/http';
 
 const initialState = {
   articles: [],
--- src/utils/helpers/http.js
+++ src/utils/helpers/http.js
@@ -1,8 +1,14 @@
-
 import axios from 'axios';
 
 export const http = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL
+  baseURL: process.env.REACT_APP_BASE_URL
 });
 
-export default http;
+export const httpWithToken = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL,
+  headers: {
+    'authorization': `token ${localStorage.getItem('token')}`,
+    'Accept' : 'application/json',
+    'Content-Type': 'application/json'
+  }
+});
